ff2ac755ff8a6b4b9364d04bd33c94ae8545f372
Canmi <9997200@qq.com>
2026-01-26 16:45:13 +0800

    feat: expose StaticLoader and DynLoader modules

--- src/loader/mod.rs
+++ src/loader/mod.rs
@@ -1,2 +1,9 @@
 /* src/loader/mod.rs */
 
+mod static_loader;
+pub use static_loader::StaticLoader;
+
+#[cfg(feature = "alloc")]
+mod dyn_loader;
+#[cfg(feature = "alloc")]
+pub use dyn_loader::DynLoader;
