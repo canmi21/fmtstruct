2fd8486efcfe27f74313421841bb49462f0ce718
Canmi <9997200@qq.com>
2026-01-26 16:21:08 +0800

    feat: establish module structure and enable no_std support

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,16 @@
 /* src/lib.rs */
 
+#![cfg_attr(not(feature = "std"), no_std)]
+
+#[cfg(feature = "alloc")]
+extern crate alloc;
+
+pub mod error;
+pub mod format;
+pub mod source;
+pub mod loader;
+
+pub use error::{FmtError, LoadResult};
+pub use format::Format;
+pub use source::Source;
+pub use loader::Loader;
