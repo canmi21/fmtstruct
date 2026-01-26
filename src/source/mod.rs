ebd75c9a6d4f0b28f8bcbe384e284d992b028045
Canmi <9997200@qq.com>
2026-01-26 16:45:13 +0800

    feat: expose MemorySource and FileSource modules

--- src/source/mod.rs
+++ src/source/mod.rs
@@ -1,2 +1,11 @@
 /* src/source/mod.rs */
 
+#[cfg(feature = "alloc")]
+mod memory;
+#[cfg(feature = "alloc")]
+pub use memory::MemorySource;
+
+#[cfg(feature = "fs")]
+mod file;
+#[cfg(feature = "fs")]
+pub use file::FileSource;
